1275190bcd97fb14b45556ebe04be63a16a0cd95
19971101 <40953074+Daydayupdate@users.noreply.github.com>
2020-07-16 13:59:21 +0800

    Update ceshi.php

--- wnejian/ceshi.php
+++ wnejian/ceshi.php
@@ -1,8 +1,8 @@
 <?php
-$arr=file('https://cdn.jsdelivr.net/gh/Daydayupdate/lyt/wnejian/images.txt');
+$arr=file('images.txt');
 $n=count($arr)-1;
 for ($i=1;$i<=1;$i++){
 $x=rand(0,$n);
-header($arr[$x],"\n");
+header("https://cdn.jsdelivr.net/gh/Daydayupdate/lyt/wnejian/".$arr[$x],"\n");
 }
 ?>
